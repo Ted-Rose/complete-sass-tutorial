f1e7e17a0c24d9f1b996c1644fc1ed0fdff58ffa
Tedis Rozenfelds <trozenfelds@gocardless.com>
2023-04-17 17:27:04 +0300

    Added comments

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,12 @@ function buildStyles() {
 
 function watchTask() {
   watch(['*.scss'], buildStyles)
+  // Asterisk * means look for any filename with this extension
+  // and compile it. So multiple sass files would be compiled.
+  // In result it also creates another file in css if there are
+  // other files.
+  // If we don't want some file to show up in css folder then
+  // add _ before the sass file name like _variables.scss
 }
 
 exports.default = series(buildStyles, watchTask)
