9acc89f9ea8a1f3c85fcde2d551965585c245e34
Tedis Rozenfelds <trozenfelds@gocardless.com>
2023-05-08 07:34:08 +0300

    Added comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
+// Needed for deleting all compiled classes that are
+// not needed.
+// !!! Remember to install gulp before!
+
+// If after cleaning index.css we add a class in HTML that
+// wasn't used before we won't be able to use that class.
+// To fix that we need to add item in wathTask to
+// automatically update css file
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
@@ -6,6 +14,8 @@ function buildStyles() {
   return src('shinobi/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(purgecss({ content: ['*.html'] }))
+    // Tells where to look for files to know what classes
+    // are used and shouldn't be deleted
     .pipe(dest('css'))
 }
 
